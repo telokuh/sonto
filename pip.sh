786814f584380a90be6946573ec94b2bd60b24c9
telokuh <telokuh@proton.me>
2025-09-20 00:58:37 +0700

    Create pip.sh

---
+++ pip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+pip install requests pyrogram python-dotenv
+# Periksa apakah variabel lingkungan $URL mengandung "mediafire"
+if [[ "$URL" == *"mediafire"* ]]; then
+    echo "URL mengandung 'mediafire'. Menjalankan pip install selenium..."
+    pip install selenium webdriver-manager
+# Jika tidak, periksa apakah mengandung "mega"
+elif [[ "$URL" == *"mega"* ]]; then
+    echo "URL mengandung 'mega'. Menjalankan pip install mega..."
+    pip install mega
+# Jika tidak ada yang cocok
+else
+    echo "URL tidak mengandung 'mediafire' atau 'mega'. Tidak ada tindakan yang diambil."
+fi
