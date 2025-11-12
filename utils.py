b5811b9466ae3c27d9fc737986d4f8b702720273
telokuh <telokuh@proton.me>
2025-11-12 22:29:11 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -338,7 +338,7 @@ def process_playwright_download(p, url, initial_message_id):
             java_script_enabled=True,
             # Playwright akan mendownload ke TEMP_DOWNLOAD_DIR jika tidak dicegat
             accept_downloads=True, 
-            downloads_path=TEMP_DOWNLOAD_DIR, 
+            
             user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         )
         page = context.new_page()
