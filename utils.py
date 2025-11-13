2f98c5c6285866344248917b0c32779551c3f9cd
telokuh <telokuh@proton.me>
2025-11-13 15:56:37 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-#from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 #import undetected_chromedriver as uc # PENTING!
 from webdriver_manager.chrome import ChromeDriverManager # <-- Diperlukan lagi
